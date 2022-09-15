ca428e3095f84466040bdc2514d1f17914a7cf9d
ack aak <vdev23102003@gmail.com>
2022-09-15 18:12:02 +0530

    Minor changes

--- Behaviour.js
+++ Behaviour.js
@@ -16,13 +16,17 @@ function GetInTouch(){
 
 function Hire(){
 
-    if(document.getElementById("HireButton").innerHTML != "Go Back" && document.getElementById("client-mail").value != ""){
-    document.getElementById("Success").setAttribute("style", "margin-left: 47%; font-size: xx-large; color: rgb(0, 255, 0); font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; display: inline;");
-    document.getElementById("HireButton").innerHTML = "Go Back";
-    sendEmail(document.getElementById("client-mail"));
-    document.getElementById("client-mail").value = "";
+    var hireButton = document.querySelector("#HireButton");
+    var client_text_field = document.querySelector("#client-mail");
+    var Success = document.querySelector("#Success");
+
+    if(hireButton.innerHTML != "Go Back" && client_text_field.value != ""){
+    Success.setAttribute("style", "margin-left: 47%; font-size: xx-large; color: rgb(0, 255, 0); font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; display: inline;");
+    hireButton.innerHTML = "Go Back";
+    sendEmail(client_text_field.value);
+    client_text_field.value = "";
     }
-    else if(document.getElementById("HireButton").innerHTML == "Go Back"){
+    else if(hireButton.innerHTML == "Go Back"){
         document.location.href = "index.html";
     }
     else{
